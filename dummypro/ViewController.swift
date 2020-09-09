3e8df8c1e16ff401b61493d9762df4b1f4b888ce
きゅん <kyun.anothercastle@gmail.com>
2020-09-09 20:47:33 +0900

    Initial Commit

---
+++ dummypro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dummypro
+//
+//  Created by きゅん on 2020/09/09.
+//  Copyright © 2020 きゅん. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
